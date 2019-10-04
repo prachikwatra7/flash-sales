2f198fb1d9297bb1ccd1502ab0c489fea855533d
prachikwatra7 <prachikwatra7@gmail.com>
2019-10-04 12:27:49 +0000

    complete files

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get root 'pages#home'
+#   get 'pages/home', to: 'pages#home'
+  get 'pages/login', to: 'pages#login'
+  
+end
